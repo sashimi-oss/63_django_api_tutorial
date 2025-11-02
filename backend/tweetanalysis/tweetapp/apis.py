4c6125f3b8e230b7f488074a96b6362194569149
sashimi-oss <piagita7@gmail.com>
2025-11-02 23:37:14 +0900

    first commit

---
+++ backend/tweetanalysis/tweetapp/apis.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets, routers
+from myapp.models import Article # 修正必要
+from myapp.serializers import ArticleSerializer
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+router = routers.DefaultRouter()
+router.register(r'articles', ArticleViewSet)
